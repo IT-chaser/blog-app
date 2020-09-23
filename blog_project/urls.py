372b33f2496857e2c415359be6cf7f96b1c6b874
John <john@email.com>
2020-09-23 20:00:58 +0900

    forms and user accounts

--- blog_project/urls.py
+++ blog_project/urls.py
@@ -18,5 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accoutns/', include('django.contrib.auth.urls')),
+    path('accoutns/', include('accounts.urls')),
     path('', include('blog.urls')),
 ]
